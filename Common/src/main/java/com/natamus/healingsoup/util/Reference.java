7f11953c254705ef097d71e30a420ac0fb37cb80
Rick South <rick@natamus.com>
2024-05-08 15:13:14 +0200

    (v4.6) (1.20.6) Fixed the soup recipes not being parsed correctly.

--- Common/src/main/java/com/natamus/healingsoup/util/Reference.java
+++ Common/src/main/java/com/natamus/healingsoup/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.healingsoup.util;
 public class Reference {
 	public static final String MOD_ID = "healingsoup";
 	public static final String NAME = "Healing Soup";
-	public static final String VERSION = "4.5";
+	public static final String VERSION = "4.6";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
