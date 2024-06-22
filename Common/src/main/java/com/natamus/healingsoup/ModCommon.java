85df8166ca4ccd9858c0e9949a1336815065d7c0
Rick South <rick@natamus.com>
2024-06-23 01:27:12 +0200

    (v4.8) Simplified registering items with Collective's helper functions.

--- Common/src/main/java/com/natamus/healingsoup/ModCommon.java
+++ Common/src/main/java/com/natamus/healingsoup/ModCommon.java
@@ -1,6 +1,13 @@
 package com.natamus.healingsoup;
 
+import com.natamus.collective.functions.CreativeModeTabFunctions;
+import com.natamus.collective.services.Services;
 import com.natamus.healingsoup.config.ConfigHandler;
+import com.natamus.healingsoup.items.SoupFoods;
+import com.natamus.healingsoup.items.SoupItems;
+import com.natamus.healingsoup.util.Reference;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 
 public class ModCommon {
 
@@ -10,6 +17,22 @@ public class ModCommon {
 	}
 
 	private static void load() {
-		
+
+	}
+
+	public static void registerAssets(Object modEventBusObject) {
+		Services.REGISTERITEM.registerItem(modEventBusObject, new ResourceLocation(Reference.MOD_ID, "mushroom_soup"), () -> new Item((new Item.Properties()).stacksTo(1).food(SoupFoods.MUSHROOM_SOUP)), CreativeModeTabFunctions.getCreativeModeTabResourceKey("food_and_drinks"));
+		Services.REGISTERITEM.registerItem(modEventBusObject, new ResourceLocation(Reference.MOD_ID, "cactus_soup"), () -> new Item((new Item.Properties()).stacksTo(1).food(SoupFoods.CACTUS_SOUP)), CreativeModeTabFunctions.getCreativeModeTabResourceKey("food_and_drinks"));
+		Services.REGISTERITEM.registerItem(modEventBusObject, new ResourceLocation(Reference.MOD_ID, "chocolate_milk"), () -> new Item((new Item.Properties()).stacksTo(1).food(SoupFoods.CHOCOLATE_MILK)), CreativeModeTabFunctions.getCreativeModeTabResourceKey("food_and_drinks"), true);
+	}
+
+	public static void setAssets() {
+		SoupItems.MUSHROOM_SOUP = Services.REGISTERITEM.getRegisteredItem(new ResourceLocation(Reference.MOD_ID, "mushroom_soup"));
+		SoupItems.CACTUS_SOUP = Services.REGISTERITEM.getRegisteredItem(new ResourceLocation(Reference.MOD_ID, "cactus_soup"));
+		SoupItems.CHOCOLATE_MILK = Services.REGISTERITEM.getRegisteredItem(new ResourceLocation(Reference.MOD_ID, "chocolate_milk"));
+
+		SoupItems.soups.add(SoupItems.MUSHROOM_SOUP);
+		SoupItems.soups.add(SoupItems.CACTUS_SOUP);
+		SoupItems.soups.add(SoupItems.CHOCOLATE_MILK);
 	}
 }
