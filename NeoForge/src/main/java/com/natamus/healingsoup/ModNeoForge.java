39fa7f0555486e3f28e078678415b83d7aefbdd8
Rick South <rick@natamus.com>
2024-07-28 07:29:32 +0200

    (v5.0) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/healingsoup/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/healingsoup/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.healingsoup;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.healingsoup.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.healingsoup.neoforge.events.NeoForgeSoupEvent;
 import com.natamus.healingsoup.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.neoforge.common.NeoForge;
 public class ModNeoForge {
 
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
