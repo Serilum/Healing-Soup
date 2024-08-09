3dbabdf20a1b47a7d57b7aed3ad20c9aec758c4a
Rick South <rick@natamus.com>
2024-08-09 08:20:18 +0200

    Healing Soup source code for 1.21.1.

---
+++ NeoForge/src/main/java/com/natamus/healingsoup/neoforge/events/NeoForgeSoupEvent.java
@@ -0,0 +1,17 @@
+package com.natamus.healingsoup.neoforge.events;
+
+import com.natamus.healingsoup.events.SoupEvent;
+import net.minecraft.world.InteractionResult;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeSoupEvent {
+	@SubscribeEvent
+	public static void onPlayerInteract(PlayerInteractEvent.RightClickItem e) {
+		if (SoupEvent.onPlayerInteract(e.getEntity(), e.getLevel(), e.getHand()).getResult().equals(InteractionResult.FAIL)) {
+			e.setCanceled(true);
+		}
+	}
+}
