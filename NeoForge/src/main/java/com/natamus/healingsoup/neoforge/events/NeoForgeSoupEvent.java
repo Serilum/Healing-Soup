b924675138c990d43cf901db4b731314cbaf6561
Rick South <rick@natamus.com>
2025-02-12 23:30:08 +0100

    (v5.4) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/healingsoup/neoforge/events/NeoForgeSoupEvent.java
+++ NeoForge/src/main/java/com/natamus/healingsoup/neoforge/events/NeoForgeSoupEvent.java
@@ -4,9 +4,7 @@ import com.natamus.healingsoup.events.SoupEvent;
 import net.minecraft.world.InteractionResult;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeSoupEvent {
 	@SubscribeEvent
 	public static void onPlayerInteract(PlayerInteractEvent.RightClickItem e) {
