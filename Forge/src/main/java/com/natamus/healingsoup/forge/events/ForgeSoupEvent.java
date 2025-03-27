190e119964648ef568d7c629f94aed35ee9cf45f
Rick South <rick@natamus.com>
2025-03-28 00:46:02 +0100

    Healing Soup source code for 1.21.5.

---
+++ Forge/src/main/java/com/natamus/healingsoup/forge/events/ForgeSoupEvent.java
@@ -0,0 +1,15 @@
+package com.natamus.healingsoup.forge.events;
+
+import com.natamus.healingsoup.events.SoupEvent;
+import net.minecraft.world.InteractionResult;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeSoupEvent {
+	@SubscribeEvent
+	public static void onPlayerInteract(PlayerInteractEvent.RightClickItem e) {
+		if (SoupEvent.onPlayerInteract(e.getEntity(), e.getLevel(), e.getHand()).equals(InteractionResult.FAIL)) {
+			e.setCanceled(true);
+		}
+	}
+}
