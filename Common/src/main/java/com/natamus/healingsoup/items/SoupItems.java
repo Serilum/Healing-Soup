943ad627b9f724745d0cebfde3606672873c512b
Rick South <rick@natamus.com>
2025-10-02 12:34:53 +0200

    Healing Soup source code for 1.21.9.

---
+++ Common/src/main/java/com/natamus/healingsoup/items/SoupItems.java
@@ -0,0 +1,14 @@
+package com.natamus.healingsoup.items;
+
+import net.minecraft.world.item.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SoupItems {
+	public static List<Item> soups = new ArrayList<Item>();
+
+	public static Item MUSHROOM_SOUP;
+	public static Item CACTUS_SOUP;
+	public static Item CHOCOLATE_MILK;
+}
